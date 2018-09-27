87b32d336e72032aff9c4713525008ffcbac4fd1
Hardik Patel <hpatel@wsgc.com>
2018-09-27 12:44:01 -0700

    Adds cheat-sheet

---
+++ cheat-sheet.js
@@ -0,0 +1,61 @@
+/************************************
+ ***************** STRING
+ * 
+ *************************************/
+// Unique characters in string
+String.prototype.concat(...new Set(string))
+
+/************************************
+ ***************** MAP
+ * 
+ *************************************/
+// each value in map
+const map = new Map();
+const mapIterator = map.values();
+for (let index = 0; index < map.size; index++) {
+    //do something
+}
+
+// Get unique (distinct) element from Arr
+
+/************************************
+ ***************** ARRAY
+ * 
+ *************************************/
+// Get unique (distinct) element from Array using Set
+const uniqueNumbersSet = new Set(myArray);
+const uniqueNumbersArray = Array.from(new Set(myArray));
+
+// Max of an Array
+Math.max(...myArray);
+
+// Sort number array
+// Array.sort() sorts for string. that is why we can not use Array.sort() for sorting number. instead, 
+arr.sort((a, b) => a - b);
+
+//Array contains
+var array1 = [1, 2, 3];
+console.log(array1.includes(2));
+// expected output: true
+
+//Declare array with size
+myArray = new int[10];
+
+// Pre-fill array 
+// Pre-filling it with 0 here
+Array.from(Array(myArray.length), () => 0);
+
+
+//Reverse integer
+let number = 123;
+number = number + "";
+let reverseNumber = parseInt(number.split("").reverse().join(""));
+
+
+
+//Create alphabets
+//65 is ASCI code for A and 90 for Z
+//97 to 122 for a-z
+for (let i = 65; i <= 90; i++) {
+    console.log(String.fromCharCode(i));
+}
